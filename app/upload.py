2320af52fca8e7413e6859cc662e2d770f9070ad
Rupesh chavan <rupeshchavan48391@gmail.com>
2026-08-11 18:03:31 +0530

    Import logger in upload.py
    Added logger import for improved logging functionality.

--- app/upload.py
+++ app/upload.py
@@ -2,6 +2,8 @@
 from botocore.exceptions import ClientError
 from s3_service import s3
 from config import BUCKET_NAME
+from logger import logger
+
 
 from utils import check_file_exists, get_file_name
 
