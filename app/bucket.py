7a8a7ef80b33da748a4aa1241495576a22a9ce24
Rupesh chavan <rupeshchavan48391@gmail.com>
2026-08-11 18:37:41 +0530

    Refactor bucket.py to use logging and improve docstrings
    Updated docstrings for clarity and replaced print statements with logging.

--- app/bucket.py
+++ app/bucket.py
@@ -1,16 +1,15 @@
-"""
-=========================================
-File Name : bucket.py
-"
+
 from botocore.exceptions import ClientError
 from s3_service import s3
 from config import BUCKET_NAME, AWS_REGION
+from logger import logger
 
 
 def create_bucket():
     """
-    Creates a new S3 bucket.
+    Create the configured S3 bucket.
     """
+
     try:
 
         s3.create_bucket(
@@ -20,41 +19,74 @@ def create_bucket():
             }
         )
 
-        print(f"✅ Bucket '{BUCKET_NAME}' created successfully.")
+        logger.info(
+            f"Bucket '{BUCKET_NAME}' created successfully."
+        )
 
     except ClientError as error:
-        print(f"❌ Failed to create bucket.\n{error}")
+
+        logger.error(
+            f"Bucket creation failed: {error}"
+        )
+
+    except Exception as error:
+
+        logger.error(
+            f"Unexpected error: {error}"
+        )
 
 
 def list_buckets():
     """
-    Displays all S3 buckets available
-    in  AWS account.
+    Display all available S3 buckets.
     """
-    try:
-  response = s3.list_buckets()
-        print("\n========== S3 Buckets ==========\n")
 
-        if not response["Buckets"]:
-            print("No buckets found.")
+    try:
+
+        response = s3.list_buckets()
+
+        logger.info("Available S3 buckets:")
 
         for bucket in response["Buckets"]:
+
             print(f"• {bucket['Name']}")
 
     except ClientError as error:
-        print(error)
+
+        logger.error(
+            f"Failed to list buckets: {error}"
+        )
+
+    except Exception as error:
+
+        logger.error(
+            f"Unexpected error: {error}"
+        )
 
 
 def delete_bucket():
     """
-    Deletes the configured bucket.
+    Delete the configured S3 bucket.
     """
+
     try:
- s3.delete_bucket(
+
+        s3.delete_bucket(
             Bucket=BUCKET_NAME
         )
 
-        print(f"🗑 Bucket '{BUCKET_NAME}' deleted successfully.")
+        logger.info(
+            f"Bucket '{BUCKET_NAME}' deleted successfully."
+        )
 
     except ClientError as error:
-        print(error)
+
+        logger.error(
+            f"Bucket deletion failed: {error}"
+        )
+
+    except Exception as error:
+
+        logger.error(
+            f"Unexpected error: {error}"
+        )
