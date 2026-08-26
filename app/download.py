b012918d3b296fb700e0bea05255908e9229853a
Rupesh chavan <rupeshchavan48391@gmail.com>
2026-08-26 11:59:49 +0530

    Enhance error handling in download_file function
    Refactor download_file function to improve error handling and logging.

--- app/download.py
+++ app/download.py
@@ -1,12 +1,12 @@
-
-
 from botocore.exceptions import ClientError
+
 from s3_service import s3
 from config import BUCKET_NAME
 from logger import logger
 
+
 def download_file(file_name, download_path):
-  
+
     try:
 
         s3.download_file(
@@ -15,11 +15,30 @@ def download_file(file_name, download_path):
             download_path
         )
 
-       logger.info(f"File '{file_name}' downloaded successfully.")
-       logger.info(f"Saved to: {download_path}")
+        logger.info(
+            f"File '{file_name}' downloaded successfully."
+        )
 
-  except ClientError as error:
-    logger.error(f"Download failed: {error}")
+        logger.info(
+            f"Saved to: {download_path}"
+        )
 
-except Exception as error:
-    logger.error(f"Unexpected error: {error}")
+        return True
+
+    except ClientError as error:
+
+        logger.error(
+            f"Download failed: {error}"
+        )
+
+        return False
+
+    except Exception as error:
+
+        logger.error(
+            f"Unexpected error: {error}"
+        )
+
+        return False
+
+
